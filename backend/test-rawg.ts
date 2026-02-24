73bf792fc07bde272dedaa3f90054600957e315e
takumigreeeen625-ai <takumi.greeeen.6.25@gmail.com>
2026-02-25 03:03:04 +0900

    chore: remove test files

--- backend/test-rawg.ts
+++
@@ -1,2 +0,0 @@
-import { fetchRawgGameMetadata } from './src/services/rawg'; import * as dotenv from 'dotenv'; dotenv.config();
-(async () => { const res = await fetchRawgGameMetadata('逆転裁判123'); console.log(JSON.stringify(res, null, 2)); })();
